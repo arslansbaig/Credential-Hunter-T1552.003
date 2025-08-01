ae7bedbdfe493810c4cf331b5884f60b03204159
Arslan Baig <arslancsf@gmail.com>
2025-08-01 19:01:00 +0300

    Update T1552.003-ScriptCredentialHunter.ps1

--- Scripts/T1552.003-ScriptCredentialHunter.ps1
+++ Scripts/T1552.003-ScriptCredentialHunter.ps1
@@ -58,4 +58,3 @@ foreach ($path in $searchPaths) {
 
 Add-Content $artifactFile "Scan completed on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 Write-Output "[+] Script credential scan completed. Check $artifactFile"
-
